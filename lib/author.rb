7da5ec1ef8fb75f8f583ae1b0b340ee16d9c2e84
Daniel Matney <danieljmatney@gmail.com>
2019-10-11 03:23:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,20 +5,20 @@ class Author
     @name = name
   end
   
-  def add_song(song)
-    song.artist = self
+  def add_post(post)
+    post.artist = self
   end
   
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
+  def add_post_by_name(name)
+    post = Post.new(name)
+    post.author = self
   end
   
-  def self.song_count
-    Song.all.length
+  def self.post_count
+    Post.all.length
   end
   
-  def songs
-    Song.all.select {|song| song.artist = self}
+  def posts
+    Post.all.select {|post| post.author = self}
   end
 end
